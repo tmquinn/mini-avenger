fda503dae43579a449714f0992d5aa87b09fcb2b
Quinn C. Hoyer <quinn@themightyquinn.com>
2013-09-29 00:40:28 -0700

    whatever

--- src/web.js
+++ src/web.js
@@ -16,8 +16,6 @@ server.get('/', restify.serveStatic({
     default: 'index.html'
 }))
 
-server.get('/foo', function (res))
-
 server.listen(process.env.PORT || 8080, function() {
     console.log('%s listening at %s', server.name, server.url);
 });
